55bfe4eba6f816c18c01e2cc9b0b88240c941b10
ariel7342 <ariell@wix.com>
2019-12-02 01:35:18 +0200

    created peoject with sso

---
+++ sign_in_flutter/lib/widgets/signIn_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:sign_in_flutter/screens/first_screen.dart';
+import 'package:sign_in_flutter/services/sign_in.dart';
+
+class SinginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+        signInWithGoogle().whenComplete(() {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return FirstScreen();
+              },
+            ),
+          );
+        });
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
